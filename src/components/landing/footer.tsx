6b9c481e9a29d7dd368c2d136f4d3b1c46c5458c
Ralvy Mamonto <Fauzanbros@gmail.com>
2025-10-18 21:14:19 +0000

    text : Apinkgo Pointify Consulting (_for element span_)

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -6,14 +6,14 @@ export default function Footer() {
       <div className="container py-6 flex items-center justify-between flex-col sm:flex-row gap-4">
         <div className="flex items-center gap-2">
           <Package className="h-5 w-5" />
-          <span className="font-bold">Pointify Consulting</span>
+          <span className="font-bold">Apinkgo Pointify Consulting</span>
         </div>
         <div className="flex flex-col items-center sm:items-end gap-2">
           <p className="text-sm text-primary-foreground/70">
             NIB: 2705220053295
           </p>
           <p className="text-sm text-center sm:text-right text-primary-foreground/70">
-            © {new Date().getFullYear()} Pointify Consulting. All Rights Reserved.
+            © {new Date().getFullYear()} Apinkgo Pointify Consulting. All Rights Reserved.
           </p>
         </div>
       </div>
--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <Package className="h-6 w-6 text-primary" />
-          <span className="font-bold font-headline text-lg">Pointify Consulting</span>
+          <span className="font-bold font-headline text-lg">Apinkgo Pointify Consulting</span>
         </Link>
         <nav className="flex flex-1 items-center justify-end space-x-2">
           <Button asChild variant="ghost">
