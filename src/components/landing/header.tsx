a3f65ef3af0b56f25fca681b2bcb0c0911ea8659
Ralvy Mamonto <Fauzanbros@gmail.com>
2025-10-24 08:42:35 +0000

    besarkan 10%

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-20 max-w-screen-2xl items-center">
         <Link href="/" className="mr-6 flex items-center">
-          <Image src="/logo.svg" alt="Apinkgo Pointify Consulting Logo" width={200} height={54} priority />
+          <Image src="/logo.svg" alt="Apinkgo Pointify Consulting Logo" width={240} height={65} priority />
         </Link>
         <nav className="flex flex-1 items-center justify-end space-x-2">
           <Button asChild variant="ghost">
