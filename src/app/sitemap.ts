0801b54c780919424ac7f84be81a5cfda4448ba7
Ralvy Mamonto <Fauzanbros@gmail.com>
2025-10-30 15:36:32 +0000

    untuk SEO, buatkan link untuk sitemap yang dapat dideteksi google search

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://pointify.web.app';
+
+  return [
+    {
+      url: siteUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${siteUrl}/portfolio`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
